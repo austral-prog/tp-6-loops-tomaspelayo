ead945e8a55e7125f776b6a3c4e683609175acfd
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-05-08 11:27:23 -0300

    Update sum_range.py

--- sum_range.py
+++ sum_range.py
@@ -7,7 +7,12 @@ def sum_to_n(n):
 
     Ejemplo: sum_to_n(5) -> 15  (1+2+3+4+5)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if n <= 0:
+        return 0
+    total = 0
+    for i in range(1, n+1):
+        total += i
+    return total
 
 
 def sum_evens(n):
@@ -17,7 +22,12 @@ def sum_evens(n):
 
     Ejemplo: sum_evens(10) -> 30  (2+4+6+8+10)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if n <= 0:
+        return 0
+    total = 0
+    for i in range(2 ,n + 1, 2):
+        total += i
+    return total
 
 
 def factorial(n):
@@ -27,4 +37,9 @@ def factorial(n):
 
     Ejemplo: factorial(5) -> 120  (1*2*3*4*5)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if n <= 0:
+        return 1
+    total = 1
+    for i in range(1,n+1):
+        total *= i
+    return total
